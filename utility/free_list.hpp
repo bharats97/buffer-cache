c7d80a4b27fe742823dd4a894c8e56d8079b00f0
bharats97 <bharatsardana347@gmail.com>
2020-04-15 04:17:42 +0530

    updates free_list utility

--- utility/free_list.hpp
+++ utility/free_list.hpp
@@ -4,13 +4,37 @@
     
     #include <list>
     
-    #include "utility/buffer.hpp"
+    #include <cassert>
+
+    #include "buffer.hpp"
     
     struct free_list {
         
-        std::list<buffer::header*> lst;
+        std::list<buffer::header> lst;
+
+        long unsigned int capacity;
         
-        inline free_list(int n) : lst(n) {}
+        inline free_list (int n) : capacity(n){}
+
+        inline std::list<buffer::header>::iterator head () {
+        	assert(!lst.empty());
+        	return lst.begin ();
+        }
+
+        std::list<buffer::header>::iterator add(buffer::header buf) {
+        	assert((lst.size() != capacity) && !buf.status.locked);
+        	lst.push_back(buf);
+        	buf.status.locked = false;
+        	std::list<buffer::header>::iterator it = lst.end();
+        	it--;
+        	return it;
+        }
+
+        buffer::header remove(std::list<buffer::header>::iterator it) {
+        	buffer::header buf = *it;
+        	lst.erase(it);
+        	return buf;
+        }
         
     };
 
